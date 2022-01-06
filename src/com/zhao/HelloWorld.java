294559cd620bdfd01349895fe015ff2cad4cce56
zhao <773086084@qq.com>
2022-01-06 18:14:29 +0800

    push commit

--- src/com/zhao/HelloWorld.java
+++ src/com/zhao/HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
         System.out.println("hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
